329ee7a0cada6f0186e7a658e5e5b239b01e04c3
Pedro César <pedrodhin@gmail.com>
2022-07-11 22:39:21 -0300

    LifeClycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <PrimeiroComponente />
+  <div>
+    <PrimeiroComponente />
+    <p>
+      o dado de app é: {{ testando }}
+    </p>
+    <LifeCycle />
+  </div>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
 
 export default {
   name: 'App',
   components: {
-    PrimeiroComponente
+    PrimeiroComponente,
+    LifeCycle
+  },
+  data() {
+    return {
+      testando: "Teste"
+    }
   }
 }
 </script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+            nome: 'Ainda não sei'
+        }
+    },
+    created() {
+        setTimeout(() => {
+            this.nome = 'Pedro'
+        }, 1000)       
+    },
+    mounted() {
+        setTimeout(() => {
+            this.nome = 'Pedro César'
+        }, 2000)
+    }
+
+}
+</script>
