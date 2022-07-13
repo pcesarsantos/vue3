352f981255d4705026f733e54ee8ff73a1712fa5
Pedro César <pedrodhin@gmail.com>
2022-07-13 17:14:28 -0300

    v-for

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,11 +2,13 @@
     <div>
         <p v-if="estaTrabalhando">Estou trabalhando no momento.</p>
         <p v-else>Estou em busca de novas oportunidades</p>
-        <p>utilizo as seguintes tecnologias:</p>
+        <p>utilizo as seguintes tecnologias para backend:</p>
         <ul>
-            <li>Javascript</li>
-            <li>PHP</li>
-            <li>C#</li>
+            <li v-for="(technology, idx) in backendTechnologies" :key="idx">{{ technology }}</li>
+        </ul>
+        <p>utilizo as seguintes tecnologias para frontend:</p>
+        <ul>
+            <li v-for="technology in frontendTechnologies" :key="technology.id">{{ technology.language }}</li>
         </ul>
         <div>
             <button @click="showEmail">Mostrar e-mail</button>
@@ -28,7 +30,13 @@ export default {
             estaTrabalhando: false,
             mostrarEmail: false,
             email: 'pcsantos@outlook.com.br',
-            meuLink: 'http://www.github.com/pcesarsantos'
+            meuLink: 'http://www.github.com/pcesarsantos',
+            backendTechnologies: ['JavaScript','PHP', 'C#'],
+            frontendTechnologies: [
+                { id: 1, language: 'HTML'},
+                { id: 2, language: 'CSS'},
+                { id: 3, language: 'VUE' }
+            ]
         }
     },
     methods: {
