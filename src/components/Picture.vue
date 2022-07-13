9d4978dbaea3d7601db4e459207343193e6e3c9c
Pedro César <pedrodhin@gmail.com>
2022-07-13 20:50:41 -0300

    Emit

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,20 +1,25 @@
 <template>
     <div>
         <img :src="avatar" :alt="descricao" />
-        <Reutilizacao />
+        <MudarImagem @mudarImagem="trocarImagem" />
     </div>
 </template>
 <script>
-import Reutilizacao from './Reutilizacao.vue'
+import MudarImagem from './MudarImagem.vue'
 export default {
     name: 'myPicture',
     components: {
-        Reutilizacao
+        MudarImagem
     },
     data() {
         return {
             avatar: "/img/avatar.jpg",
             descricao: "Pedro César"
+        }
+    },
+    methods: {
+        trocarImagem() {
+            this.avatar = '/img/avatar2.png'
         }
     }
 }
