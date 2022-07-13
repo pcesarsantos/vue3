005b5c261ce913c1409d52f542116464f660d07d
Pedro César <pedrodhin@gmail.com>
2022-07-13 19:58:47 -0300

    Multiplos eventos

---
+++ src/components/MultiplosEventos.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <button @click="primeiro(), segundo(), terceiro()">Ativar Multiplos Eventos</button>
+        <p> {{ multiplosEventos }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MultiplosEventos",
+    data() {
+        return {
+            multiplosEventos: ""
+        }
+    },
+    methods: {
+        primeiro() {
+            this.multiplosEventos = "Primeiro evento ativado"
+        },
+        async segundo() {
+            setTimeout(() => { this.multiplosEventos = "Segundo Evento ativado"}, 500)
+        },
+        async terceiro() {
+            setTimeout(() => { this.multiplosEventos = ""}, 1500)
+        }
+    }
+}
+</script>
+<style scoped>
+div {
+    margin: 100px;
+}
+</style>
