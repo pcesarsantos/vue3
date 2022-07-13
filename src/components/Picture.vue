8c636a72c94dad40eaa588dcafdbd3ed3102f001
Pedro César <pedrodhin@gmail.com>
2022-07-13 20:17:05 -0300

    Reutilização de componentes

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,9 +1,16 @@
 <template>
-    <img :src="avatar" :alt="descricao">
+    <div>
+        <img :src="avatar" :alt="descricao" />
+        <Reutilizacao />
+    </div>
 </template>
 <script>
+import Reutilizacao from './Reutilizacao.vue'
 export default {
     name: 'myPicture',
+    components: {
+        Reutilizacao
+    },
     data() {
         return {
             avatar: "/img/avatar.jpg",
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,18 +1,23 @@
 <template>
     <div>
+        <Reutilizacao />
         <h1>Olá Vue!</h1>
+        <Reutilizacao />
         <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
         <multiplosEventos />
+        <Reutilizacao />
     </div>
 </template>
 
 <script>
  import MultiplosEventos from './MultiplosEventos.vue'
+import Reutilizacao from './Reutilizacao.vue'
 
 export default {
     name: 'PrimeiroComponente',
     components: {
-        MultiplosEventos
+        MultiplosEventos,
+        Reutilizacao
     },
     data() {
         return {
